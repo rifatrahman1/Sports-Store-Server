dcb878f8f6051dfd879353bbb638699fc3e3f576
Rifat Rahman <ahsan30rifat@gmail.com>
2025-03-04 23:43:28 +0600

    added to sports store server basic setup

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASS);
+ console.log(process.env.DB_USER);
+ console.log(process.env.DB_PASS);
 
-const uri = "mongodb+srv://rifatrahman1:bu0ZYzunbgapHKFN@cluster0.noaml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.noaml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
