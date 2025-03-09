fede20162503219ec5cb9332fd2ec30562750723
Rifat Rahman <ahsan30rifat@gmail.com>
2025-03-09 16:10:18 +0600

    added to server site code sorted price added

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.noaml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -27,17 +25,24 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const user_sports = client.db('sportsDB').collection('sports');
     const user_login = client.db('sportsDB').collection('users');
 
     // users sports crud
     app.get('/sports', async (req, res) => {
+      const cursor = user_sports.find().limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/all_sports', async (req, res) => {
       const cursor = user_sports.find();
       const result = await cursor.toArray();
       res.send(result);
     })
+  
 
     app.get('/sports/:id', async (req, res) => {
       const id = req.params.id;
